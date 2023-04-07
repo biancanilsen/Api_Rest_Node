bd1e7941ef4620c26e8a3621cea044c347f46f68
Bianca Nilsen <86383966+biancanilsen@users.noreply.github.com>
2023-04-07 17:22:12 -0300

    Update mysql.js

--- mysql.js
+++ mysql.js
@@ -9,22 +9,3 @@ var pool = mysql.createPool({
 });
 
 exports.pool = pool;
-
-//  const mysql = require ('mysql2');
-
-//  var connection = mysql.createConnection({
-//     host : 'localhost',
-//     database : 'study',
-//     user: 'root',
-//     password: '170601'
-//  });
-
-//  connection.connect(function (error){
-//     if(error){
-//         throw error;
-//     }
-//     else{
-//         console.log('MySQL Database is connected Succcesfully')
-//     }
-//  });
-//  module.exports = connection;
